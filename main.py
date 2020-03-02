90f276fe74feb65db31f0c55a53d8e2f21667569
Andres Vieira <anvieiravazquez@gmail.com>
2020-03-03 00:00:07 +0100

    Main: Engade un manexador para dicirlle adios a un usuario
    Este manexador dille "Talogo $USUARIO" ao usuario cando escribe o
    comando "/bye" nun chat co bot engadido.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from telegram.ext import Updater, CommandHandler
 
 
-def hello(update, context):
+def hello_handler(update, context):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
+
+def bye_handler(update, context):
+    update.message.reply_text("Talogo {}".format(update.message.from_user.first_name))
 
 
 updater = Updater('1142606683:AAFHEBgNIDbqZZ6RizWYRLFmqRpr-8KRBEg', use_context=True)
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('hello', hello_handler))
+updater.dispatcher.add_handler(CommandHandler('bye', bye_handler))
 
 updater.start_polling()
 updater.idle()
