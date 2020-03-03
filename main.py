96f320e97de8fdcf875beee45cc6464fbb30781f
Andres Vieira <anvieiravazquez@gmail.com>
2020-03-03 21:51:06 +0100

    Main: Refactoriza o bot para esperar un /begin para iniciar un xogo
    Antes deste parche o bot creaba unha partida ao arrancar. Pero
    isto vainos a dar bastantes problemas nun futuro por que non
    sabemos que usuario inicia a partida, ergo non sabemos
    a quen deixarlle mover o xogador e a quen non.

--- main.py
+++ main.py
@@ -11,20 +11,37 @@ logging.basicConfig(level=logging.DEBUG,
 
 updater = Updater('1142606683:AAFHEBgNIDbqZZ6RizWYRLFmqRpr-8KRBEg', use_context=True)
 
-a_board = Board()
-an_enemy = Enemy("Ogro")
-a_player = Player(12,"Guerrero")
+# Game data
+a_board = None
+an_enemy = None
+a_player = None
 
-a_board.cells[0][0] = a_player
-a_board.cells[9][9] = an_enemy
+
+def begin_game(update, context):
+    global a_board
+    global an_enemy
+    global a_player
+
+    a_board = Board()
+    an_enemy = Enemy("Ogro")
+    a_player = Player(12, "Guerrero")
+    a_board.cells[0][0] = a_player
+    a_board.cells[9][9] = an_enemy
+
+    update.message.reply_text("I've created a new game! Please use /show to show the board.")
 
 
 def draw_board(update, context):
-	update.message.reply_text(str(a_board))
+    if a_board == None:
+        update.message.reply_text("There is no board created yet. Please use the command /begin to begin a new game. 💘")
+    else:
+        update.message.reply_text(str(a_board))
+
 
 updater.dispatcher.add_handler(CommandHandler('show', draw_board))
+updater.dispatcher.add_handler(CommandHandler('begin', begin_game))
 
 updater.start_polling()
 updater.idle()
 
-#<3
+# <3
